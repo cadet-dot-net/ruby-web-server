d78a5c1bc75d21aa0375a5faedc7a7d47a5d98b6
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2025-02-28 11:45:02 +0000

    add response handlers to serve files from the directory

--- lib/server.rb
+++ lib/server.rb
@@ -2,12 +2,37 @@ require 'socket'
 require './lib/response'
 
 PORT = 8081
+SERVER_ROOT = "/tmp/web-server/"
+
 server = TCPServer.new('0.0.0.0', PORT)
 
 def handle_connection(client)
   request = client.readpartial(2048)
   client.close
-  puts request
+end
+
+def prepare_response(request)
+  if request.fetch(:path) == "/"
+    respond_with(SERVER_ROOT + "index.html")
+  else
+    respond_with(SERVER_ROOT + request.fetch(:path))
+  end
+end
+
+def respond_with(path)
+  if File.exist?(path)
+    send_ok_response(File.binread(path))
+  else
+    send_file_not_found
+  end
+end
+
+def send_ok_response(data)
+  Response.new(code: 200, data: data)
+end
+
+def send_file_not_found
+  Response.new(code: 404)
 end
 
 def parse(request)
