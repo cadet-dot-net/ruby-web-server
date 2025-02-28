ef65a3065d54466788fd41e1bf80050d4ce036e4
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2025-02-28 09:19:02 +0000

    parse the request headers

--- lib/server.rb
+++ lib/server.rb
@@ -5,11 +5,35 @@ server = TCPServer.new('0.0.0.0', PORT)
 
 def handle_connection(client)
   request = client.readpartial(2048)
-
-  # client.write("Hello from server")
   client.close
-
   puts request
+end
+
+def parse(request)
+  method, path, version = request.lines[0].split
+  
+  {
+    path: path,
+    method: method,
+    headers: parse_headers(request)
+  }
+end
+
+def parse_headers(request)
+  headers = {}
+
+  request.lines[1..-1].each do |line|
+    return headers if line == "\r\n"
+
+    header, value = line.split
+    header = normalise(header)
+
+    headers[header] = value
+  end
+end
+
+def normalise(header)
+  header.gsub(":", "").downcase.to_sym
 end
 
 puts "Listening on #{PORT}. Press CTRL+C to cancel."
